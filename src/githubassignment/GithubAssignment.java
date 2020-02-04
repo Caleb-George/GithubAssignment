33aa4fb9bd79587cb4b741fcdc698ddef796cae5
CRG15@students.ptcollege.edu <>
2020-02-04 15:17:40 -0500

    this is the first commit

---
+++ src/githubassignment/GithubAssignment.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package githubassignment;
+
+/**
+ *
+ * @author mikun
+ */
+public class GithubAssignment {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        George();
+    }
+    
+    public static void George(){
+        System.out.println("Caleb George");
+    }
+    
+}
